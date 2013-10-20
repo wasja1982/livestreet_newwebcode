e696a096a5300aa1ceaec6f6e8f29d3a2cf3e528
wasja1982 <wasja@yandex.ru>
2013-10-20 10:44:31 +0300

    Исправлена ошибка с проверкой доступа к админке.

--- classes/actions/ActionWebcode.class.php
+++ classes/actions/ActionWebcode.class.php
@@ -4,16 +4,17 @@ class PluginWebcode_ActionWebcode extends ActionPlugin{
 	public $sMenuItemSelect = 'webcode';
 	protected $sCurrentEvent;
 	public function Init(){
-		if(!$this->User_GetUserCurrent()->isAdministrator())
+		$oUserCurrent = $this->User_GetUserCurrent();
+		if (!isset($oUserCurrent) || !$oUserCurrent->isAdministrator())
 			Router::Action('error');
 		else
 			$this->setDefaultEvent('default');
 	}
-	
+
 	public function RegisterEvent(){
 		$this->AddEvent('default','EventDefault');
 	}
-	
+
 	public function EventDefault(){
 		if(isset($_POST['webcode']))
 			$this->PluginWebcode_Webcode_Submit();
